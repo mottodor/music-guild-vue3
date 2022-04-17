312c81f029232ddfab2bfdcc7d57c9e6e62f16b4
Kantemir Shogenov <kantemir-83@yandex.ru>
2022-04-17 21:52:44 +0300

    updated Navbar, added route guard

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { projectAuth } from '@/firebase/config'
+import { onAuthStateChanged } from 'firebase/auth'
 
 // global styles
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+let app
+onAuthStateChanged(projectAuth, () => {
+    if (!app) {
+        app = createApp(App).use(router).mount('#app')
+    }
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { projectAuth } from '@/firebase/config'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/auth/LoginView'
 import SignupView from '@/views/auth/SignupView'
 import CreatePlaylistView from '@/views/playlist/CreatePlaylistView'
+
+const requireAuth = (to, from, next) => {
+    const user = projectAuth.currentUser
+    if (!user) {
+        next({ name: 'login' })
+    } else {
+        next()
+    }
+}
 
 const routes = [
     {
         path: '/',
         name: 'home',
         component: HomeView,
+        beforeEnter: requireAuth,
     },
     {
         path: '/login',
@@ -24,6 +35,7 @@ const routes = [
         path: '/playlists/create',
         name: 'createPlaylist',
         component: CreatePlaylistView,
+        beforeEnter: requireAuth,
     },
 ]
 
